c75e827a1b6b7eaa29205f3c534dc67c8c363e70
Andrew Herd <32424389+aherd2985@users.noreply.github.com>
2020-10-09 09:17:52 -0500

    Update Search_Stored_Procedures.sql
    Added schema

--- Search_Stored_Procedures.sql
+++ Search_Stored_Procedures.sql
@@ -1,5 +1,6 @@
-SELECT Name
+SELECT [Name],
+[schema] = OBJECT_SCHEMA_NAME([object_id])
 FROM sys.procedures
-WHERE OBJECT_DEFINITION(OBJECT_ID) LIKE '%SEARCH%'
+WHERE OBJECT_DEFINITION(OBJECT_ID) LIKE '%Employees%'
 order by 1
 
