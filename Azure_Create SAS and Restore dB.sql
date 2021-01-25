7f5e1ac55b94716d8e0a899963286f4c56178cb0
Andrew Herd <32424389+aherd2985@users.noreply.github.com>
2021-01-25 16:33:47 -0600

    Update Azure_Create SAS and Restore dB.sql

--- Azure_Create SAS and Restore dB.sql
+++ Azure_Create SAS and Restore dB.sql
@@ -5,7 +5,7 @@ CREATE CREDENTIAL [https://{AZURE STORAGE}.blob.core.windows.net/{AZURE CONTAINE
 
 -- Restore [dB NAME] from URL to SQL Server instance using Azure blob storage for database files  
 RESTORE DATABASE KP   
-   FROM URL = 'https://{AZURE STORAGE}.blob.core.windows.net/sassycontainer/[dB NAME].bak'   
+   FROM URL = 'https://{AZURE STORAGE}.blob.core.windows.net/{AZURE CONTAINER}/[dB NAME].bak'   
    WITH  
       MOVE '[dB NAME]_data' to 'https://{AZURE STORAGE}.blob.core.windows.net/{AZURE CONTAINER}/[dB NAME].mdf'  
      ,MOVE '[dB NAME]_log' to 'https://{AZURE STORAGE}.blob.core.windows.net/{AZURE CONTAINER}/[dB NAME].ldf'  
