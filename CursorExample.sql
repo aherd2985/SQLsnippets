f956b8a7ce6b3001b5df3dbddc589f01945091b6
Andrew Herd <32424389+aherd2985@users.noreply.github.com>
2021-04-27 10:44:26 -0500

    Create CursorExample.sql

---
+++ CursorExample.sql
@@ -0,0 +1,16 @@
+DECLARE @name VARCHAR(50) -- database name 
+
+DECLARE db_cursor CURSOR FOR 
+SELECT name 
+FROM MASTER.dbo.sysdatabases 
+WHERE name NOT IN ('master','model','msdb','tempdb') 
+
+OPEN db_cursor  
+FETCH NEXT FROM db_cursor INTO @name  
+
+WHILE @@FETCH_STATUS = 0  
+BEGIN  
+      PRINT @name 
+
+      FETCH NEXT FROM db_cursor INTO @name 
+END
