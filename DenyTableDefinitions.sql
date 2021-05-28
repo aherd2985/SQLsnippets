a2750837785f9e2b173b6b151c0804960c704c93
Andrew Herd <32424389+aherd2985@users.noreply.github.com>
2021-05-28 15:55:11 -0500

    Update DenyTableDefinitions.sql

--- DenyTableDefinitions.sql
+++ DenyTableDefinitions.sql
@@ -5,7 +5,8 @@ SELECT
 	, o.type
 FROM sys.objects o
 inner join sys.schemas s on o.schema_id = s.schema_id
-WHERE o.type = N'P'
+WHERE o.type = N'U'
+-- o.type = N'P'
  --   OR o.type = N'V'
 ORDER BY o.type
     , o.name;
